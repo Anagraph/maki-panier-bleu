41ad786f259b8f2099ad5137b9bfcee9d42664e4
Jean-Denis Giguère <jean-denis@anagraph.io>
2020-05-06 08:46:17 -0400

    All icons in PNG

--- pngs/convert.sh
+++ pngs/convert.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-convert -background none ../svgs/grocery-15.svg -resize 25  grocery-25.png
+
+for ICON in grocery convenience mountain art-gallery marker bar lodging commercial clothing-store paint restaurant circle-stroked volleyball
+do
+  convert -background none ../svgs/$ICON-11.svg -resize 11  $ICON-11.png
+  convert -background none ../svgs/$ICON-15.svg -resize 15  $ICON-15.png
+  convert -background none ../svgs/$ICON-15.svg -resize 20  $ICON-20.png
+  convert -background none ../svgs/$ICON-15.svg -resize 25  $ICON-25.png
+  convert -background none ../svgs/$ICON-15.svg -resize 30  $ICON-30.png
+done
